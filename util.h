d4d8c6e813b45068c3c3479ae8339403460ee589
Stefan Isak <isak.stefan1@aon.at>
2020-02-25 13:45:15 +0100

    Util comments

--- util.h
+++ util.h
@@ -1,3 +1,4 @@
+/// @file util.h
 #pragma once
 
 #include <vector>
@@ -5,6 +6,7 @@
 
 namespace IsakMath {
     namespace Util {
+        /// Converts a vector of type X to a vectory of type Y using a regular cast
         template <typename S, typename D>
         std::vector<D> convert_vector(std::vector<S> &v) {
             std::vector<D> ret;
@@ -14,6 +16,7 @@ namespace IsakMath {
             return ret;
         }
 
+        /// Reverses a given vector
         template <typename T>
         std::vector<T> reverse(std::vector<T> &&a) {
             for (int i = 0; i < a.size()/2; i++) {
